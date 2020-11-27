f7e93d650c9800adf2bbfc2da0a147c1e98fd379
Thor G Erismann <thor.erismann@gmail.com>
2020-11-27 22:37:53 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+import sys
+def testy(did_pass):
+    linenum = sys._getframe(1).f_lineno  # Get the caller's line number.
+    if did_pass:
+        msg = "Test at line {0} ok.".format(linenum)
+    else:
+        msg = ("Test at line {0} FAILED.".format(linenum))
+    print(msg)
